e76dfc82e2eb86ba2f4c908e997c139fb1258698
logan <gengjiarong@xiaoice.com>
2024-06-15 11:30:22 +0800

    Update button_widget.dart

--- lib/components/buttons/button_widget.dart
+++ lib/components/buttons/button_widget.dart
@@ -28,19 +28,20 @@ class ButtonWidget extends StatelessWidget {
   final Widget? iconWidget;
   final String? type;
   final double? height;
+  final double? fontSize;
 
-  const ButtonWidget(
-      {required this.btnText,
-      this.onTap,
-      this.color,
-      this.borderRadius,
-      this.borderColor,
-      this.textColor,
-      this.iconWidget,
-      this.type,
-      this.borderWidth,
-      this.height,
-      super.key});
+  const ButtonWidget({required this.btnText,
+    this.onTap,
+    this.color,
+    this.borderRadius,
+    this.borderColor,
+    this.textColor,
+    this.iconWidget,
+    this.type,
+    this.borderWidth,
+    this.height,
+    this.fontSize,
+    super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -66,7 +67,7 @@ class ButtonWidget extends StatelessWidget {
                 style: TextStyle(
                     color: textColor ??
                         (type != null ? Colors.white : ButtonColors.normal),
-                    fontSize: 14),
+                    fontSize: fontSize ?? 14),
               )
           ],
         ),
@@ -79,8 +80,8 @@ class ButtonWidget extends StatelessWidget {
         border: (borderColor != null
             ? Border.all(color: borderColor!, width: borderWidth ?? 0)
             : (type == null
-                ? Border.all(color: ButtonColors.normal, width: 1)
-                : null)),
+            ? Border.all(color: ButtonColors.normal, width: 1)
+            : null)),
         color: color ?? getTypeColor(type),
         borderRadius: BorderRadius.circular(borderRadius ?? 4));
   }
